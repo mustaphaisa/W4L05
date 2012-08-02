be309b6ce14bd38fce5c20090def04cb5d2ef5b5
Mustapha <misa@brandeis.edu>
2012-08-02 08:13:31 -0400

    W3L04

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,21 @@
 class User < ActiveRecord::Base
-  attr_accessible :account, :email, :name, :username
+  attr_accessible :account, :email, :name, :username, :password, :password_confirmation
 
   has_many :narratives
   has_many :events, :through => :narratives
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :name, presence: true
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, 
+  uniqueness: { case_sensitive: false }
+  validates :username, presence: true, length: { minimum: 6, maximum: 15 }, 
+  uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :password_confirmation, presence: true
+
+  has_secure_password
+
+  before_save { |user| user.email = email.downcase }
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120730142448) do
+ActiveRecord::Schema.define(:version => 20120802115643) do
 
   create_table "events", :force => true do |t|
     t.string   "name"
@@ -34,8 +34,11 @@ ActiveRecord::Schema.define(:version => 20120730142448) do
     t.string   "username"
     t.string   "email"
     t.string   "account"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
+    t.string   "password_digest"
   end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
 
 end
